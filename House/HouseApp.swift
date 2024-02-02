e486cab7b05d50cc2f42d0b05eb33fa4b9e2e78f
vuinishiusu <ewerton.viniciusturco@gmail.com>
2024-02-02 13:18:54 -0300

    Initial Commit

---
+++ House/HouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseApp.swift
+//  House
+//
+//  Created by vuinishiusu on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

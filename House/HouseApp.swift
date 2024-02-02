2c2bc42c8ed3d02cc256bc3cbb99bd6404ee84c6
vuinishiusu <ewerton.viniciusturco@gmail.com>
2024-02-02 13:32:35 -0300

    feat: menuExtraBar and new windows for forms and preferences

--- House/HouseApp.swift
+++ House/HouseApp.swift
@@ -1,17 +1,23 @@
-//
-//  HouseApp.swift
-//  House
-//
-//  Created by vuinishiusu on 02/02/24.
-//
-
 import SwiftUI
 
 @main
 struct HouseApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("HouseApp", systemImage: "house.lodge.fill") {
+            AppMenuView()
         }
+        .menuBarExtraStyle(.window)
+
+        Window("New Door", id: "form") {
+            FormView()
+                .frame(width: 350, height: 200, alignment: .center)
+        }
+        .windowResizability(.contentSize)
+
+        Window("Preferences", id: "preferences") {
+            PreferencesView()
+                .frame(width: 350, height: 200, alignment: .center)
+        }
+        .windowResizability(.contentSize)
     }
 }
